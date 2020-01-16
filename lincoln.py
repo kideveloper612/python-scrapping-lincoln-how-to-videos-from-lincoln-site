8c4b942eaa0c5beee76edc005d23284fec83ddf1
kideveloper612 <developer61510@gmail.com>
2020-01-16 18:41:02 +0800

    lincoln how to videos download

---
+++ lincoln.py
@@ -0,0 +1,125 @@
+import requests
+import os
+import csv
+
+sections = ['Dash & Interior', 'SYNC & Technology', 'Safety & Security', 'Under the Hood & Mechanics', 'Vehicle Care', 'Vehicle Exterior']
+sub_sections = {
+    'Dash & Interior': ['Comfort', 'Interior Features', 'Interior Technology'],
+    'SYNC & Technology': ['Convenience', 'Entertainment', 'Mobile & Apps', 'SYNC', 'Safety'],
+    'Safety & Security': ['Safety Equipment', 'Safety Features', 'Security'],
+    'Under the Hood & Mechanics': ['Battery', 'Engine', 'Fluids', 'Mechanical'],
+    'Vehicle Care': ['Exterior Care', 'Interior Care', 'Vehicle Health'],
+    'Vehicle Exterior': ['Equipment', 'Lighting', 'Mechanical', 'Wheels']
+}
+
+def write_csv(lines, filename):
+    """
+    Write lines to csv named as filename
+    """
+    if not os.path.isdir('output'):
+        os.mkdir("output")
+    file_path = "output/%s" % filename
+    with open(file_path, 'a', encoding='utf-8', newline='') as writeFile:
+        writer = csv.writer(writeFile, delimiter=',')
+        writer.writerows(lines)
+
+def excute_each_request():
+    csv_header = [['YEAR', 'MAKE', 'MODEL', 'SECTION', 'SUB_SECTION', 'SYNC_VERSION', 'TITLE', 'DESCRIPTION', 'THUMBNAIL_URL', 'VIDEO_URL']]
+    filename = "Lincoln_how_to_video.csv"
+    write_csv(csv_header, filename)
+    def get_section_url(section):
+        if '&' in section:
+            sub_names = section.split(" ")
+            sub_url = ''
+            for sub_name in sub_names:
+                if sub_name is '&':
+                    sub_url = sub_url + '-' + 'and'
+                else:
+                    sub_url = sub_url + '-' + sub_name.lower()
+        else:
+            sub_names = section.split(" ")
+            sub_url = ''
+            for sub_name in sub_names:
+                sub_url = sub_url + '-' + sub_name.lower()
+        return sub_url
+
+    def get_subsection_url(subsection):
+        sub_names = subsection.split(" ")
+        sub_url = ''
+        for sub_name in sub_names:
+            if sub_name is '&':
+                sub_name = 'and'
+            sub_url = "%s-%s" % (sub_url, sub_name.lower())
+        return sub_url
+
+    def json_request(url):
+        res = requests.get(url=url).json()
+        return res
+
+    make = 'Lincoln'
+    results = []
+    for section in sections:
+        section_url = get_section_url(section)
+        for sub_section in sub_sections[section]:
+            sub_section_url = get_subsection_url(sub_section)
+            url = "https://owner.lincoln.com/support/lincoln/_jcr_content.cxht" + section_url + sub_section_url + '.json'
+            print(url.strip())
+            json_data = json_request(url)
+            records = json_data['results']
+            for record in records:
+                title = record['title']
+                description = ''
+                if 'description' in record:
+                    description = record['description']
+                video_url = "https://owner.lincoln.com" + record['contentPath'] + '.html'
+                thumbnail_url = "https://owner.lincoln.com" + record['pageThumbnail']
+                tags = record['tags']
+                for tag in tags:
+                    vehicle_specify = tag.split("/")
+                    print(vehicle_specify)
+                    if (len(vehicle_specify) != 4):
+                        try:
+                            int(vehicle_specify[1])
+                            year = vehicle_specify[1]
+                            model = vehicle_specify[3]
+                            sync_version = vehicle_specify[4]
+                            line = [year, make, model, section, sub_section, sync_version, title, description, thumbnail_url, video_url]
+                            print(line)
+                            results.append(line)
+                            write_csv([line], filename)
+                        except:
+                            year = ''
+                            model = ''
+                            sync_version = ''
+                            line = [year, make, model, section, sub_section, sync_version, title, description, thumbnail_url, video_url]
+                            print(line)
+                            results.append(line)
+                            write_csv([line], filename)
+                    if (len(vehicle_specify) == 4):
+                        try:
+                            int(vehicle_specify[1])
+                            year = vehicle_specify[1]
+                            model = vehicle_specify[3]
+                            sync_version = ''
+                            line = [year, make, model, section, sub_section, sync_version, title, description, thumbnail_url, video_url]
+                            print(line)
+                            results.append(line)
+                            write_csv([line], filename)
+                        except:
+                            year = ''
+                            model = ''
+                            sync_version = ''
+                            line = [year, make, model, section, sub_section, sync_version, title, description, thumbnail_url, video_url]
+                            print(line)
+                            results.append(line)
+                            write_csv([line], filename)
+                    elif (vehicle_specify[1] == 'all'):
+                        continue
+
+    sorted_result = sorted(results, key=lambda x: (x[0], x[2]), reverse=True)
+    write_csv([['YEAR', 'MAKE', 'MODEL', 'SECTION', 'SUB_SECTION', 'SYNC_VERSION' 'TITLE', 'DESCRIPTION', 'THUMBNAIL_URL', 'VIDEO_URL']], "Lincoln_how_to_video_Sorted.csv")
+    write_csv(sorted_result, "Lincoln_how_to_video_Sorted.csv")
+
+print('------------------------------Start------------------------------------')
+excute_each_request()
+print('------------------------------The End----------------------------------')
---
+++ video_download.py
@@ -0,0 +1,60 @@
+import csv
+
+def video_from_file(path, filename, results):
+    def write_csv(lines, filename):
+        """
+        Write lines to csv named as filename
+        """
+        import os
+        import csv
+        if not os.path.isdir('download'):
+            os.mkdir("download")
+        file_path = "download/%s" % filename
+        with open(file_path, 'a', encoding='utf-8', newline='') as writeFile:
+            writer = csv.writer(writeFile, delimiter=',')
+            writer.writerows(lines)
+
+    def dom_parser(URL):
+        import requests
+        res = requests.get(url=URL).text
+
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(res, 'html.parser')
+        for item in soup.find_all():
+            if "data-video-id" in item.attrs:
+                return item["data-video-id"]
+        return
+
+    write_csv(lines=results, filename=filename)
+    with open(path, "r", encoding="utf-8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        video_urls = {}
+        for row in csv_reader:
+            if line_count < 1:
+                line_count += 1
+            else:
+                print(line_count, row[9])
+                if not row[9] in video_urls:
+                    video_id = dom_parser(URL=row[9])
+                    if video_id is not None:
+                        video_download_url = "http://players.brightcove.net/2379864788001/B1eFuwPXkG_default/index.html?videoId=%s" % video_id
+                        row.append(video_download_url)
+                        print(row)
+                        write_csv(lines=[row], filename=filename)
+                        video_urls[row[9]] = video_download_url
+                else:
+                    video_download_url = video_urls[row[9]]
+                    row.append(video_download_url)
+                    print(row)
+                    write_csv(lines=[row], filename=filename)
+                line_count += 1
+
+    return
+
+print("--------------------Start--------------------------")
+results = [['YEAR', 'MAKE', 'MODEL', 'SECTION', 'SUB_SECTION', 'TITLE', 'DESCRIPTION', 'THUMBNAIL_URL', 'VIDEO_URL', 'VIDEO_DOWNLOAD_URL']]
+file_path = 'output/Lincoln_how_to_video_Sorted.csv'
+filename = "Lincoln_how_to_video_download.csv"
+video_from_file(path=file_path, filename=filename, results=results)
+print("--------------------The End--------------------------")
